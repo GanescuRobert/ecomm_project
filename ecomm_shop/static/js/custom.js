a06fecf4c91244536d720edafc1d14f929e6ae45
GanescuRobert <studentGanescuRobertFlorinel@gmail.com>
2022-03-10 12:48:37 +0200

    Public images, css, js

---
+++ ecomm_shop/static/js/custom.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('.increment-btn').on("click", function (e) {
+        e.preventDefault();
+        let inc_value = $(this).closest('.product_data').find('.qty-input').val();
+        let value = Number(inc_value)
+        value = isNaN(value) ? 0 : value;
+        if (value < 10) {
+            value++;
+            $(this).closest('.product_data').find('.qty-input').val(value);
+        }
+    });
+    $('.decrement-btn').on("click", function (e) {
+        e.preventDefault();
+        let inc_value = $(this).closest('.product_data').find('.qty-input').val();
+        let value = Number(inc_value)
+        value = isNaN(value) ? 0 : value;
+        if (value > 1) {
+            value--;
+            $(this).closest('.product_data').find('.qty-input').val(value);
+        }
+    });
+})
