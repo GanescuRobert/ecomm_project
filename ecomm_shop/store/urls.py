b99235a4831fe99295c7f97d1b72a7ea5bc35826
GanescuRobert <studentGanescuRobertFlorinel@gmail.com>
2022-03-10 10:09:15 +0200

    Fetch products by category

--- ecomm_shop/store/urls.py
+++ ecomm_shop/store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name="home"),
-    path('collections', views.collections, name='collections')
+    path('collections', views.collections, name='collections'),
+    path('collections/<str:slug>', views.collectionsview, name='collectionsview')
 ]
